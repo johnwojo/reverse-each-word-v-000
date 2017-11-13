9d0037a7afbf2e7b3872480f6bb662cb66f4af6e
John Wojciechowski <jwojo13@gmail.com>
2017-11-13 21:31:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,14 @@
 def reverse_each_word(sentence)
   sentence.split
+  sentence.each do |word|
+    sentence << word.reverse!
+
+end
+
+
+def reverse_each_word(sentence)
+  X Break the argument into an array.
+  Flip each of the words in the array.
+  Make the flipped-array into a string.
+  Return the string.
 end
