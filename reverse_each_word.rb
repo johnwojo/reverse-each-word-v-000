af815579f454a6c6293ee867ec5840d648b24de5
John Wojciechowski <jwojo13@gmail.com>
2017-11-13 21:39:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,5 @@
 def reverse_each_word(sentence)
   array = sentence.split
-  array.each do |word|
-    word.reverse!
-  end
-  setence = array.join( " " )
-end
-
-
-
-def reverse_each_word(sentence)
-  array = sentence.split
   array.collect do |word|
     word.reverse!
   end
